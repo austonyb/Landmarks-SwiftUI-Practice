a44e2fd20ac0fe9de688bba220adf287032d1f60
Auston Youngblood <95832900+austonyb@users.noreply.github.com>
2023-01-16 14:01:18 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Auston Youngblood on 1/16/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
